29f437fbad1ebe4fde7037634dbeedea41fead74
ChiragHawaldar <chiraghawaldar2003@gmail.com>
2026-03-10 20:32:21 +0530

    day 1 completed

--- day1/problem2.js
+++ day1/problem2.js
@@ -1,9 +1,7 @@
 // Write a function that capitalizes the first letter of every word in a sentence.
 
 function capitalizeWords(str) {
-    return    str.split(" ")
-                .map(word =>word[0].toUpperCase()+ word.slice(1))
-                .join(" ");
+    return str.split(" ").map(word=> word ? word[0].toUpperCase()+ word.slice(1) : "").join(" ");
 }
 
 console.log(capitalizeWords("hello world from javascript"));
